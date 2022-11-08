38d925f28156ccec904b2b8cebea17bb962d94ec
mennio <menniozikluv@gmail.com>
2022-11-08 21:05:35 +0100

    TD3 : Structure off project exercice 1 & 2

--- src/Exercice1/Main.java
+++ src/Exercice1/Main.java
@@ -1,6 +1,7 @@
+package Exercice1;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
     }
 }
